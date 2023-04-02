a0577b6add917e6e1c00f9d68fcbe0eb3dd17059
Christopher <chris@grislygrotto.nz>
2023-04-03 09:44:01 +1200

    removed reqest

--- readme.md
+++ readme.md
@@ -2,4 +2,8 @@
 
 A very, very simple program that will download from a url and print the result to stdout. If a second argument is provided, it will instead write to a file.
 
-By default, release builds are ~1mb on windows and linux (slightly larger on linux, ~1.3). After building with --release, run UPX on the result to chop off another 500kb+: `upx --best --lzma target/release/pull`
+Does not use any crates (even reqwest), just raw tcp sockets. Communicates over HTTP1.0, does not support HTTPS.
+
+Purpose is to copy this over to a machine (e.g. using base64) when the machine doesnt have curl, wget etc. Note most machines have some programming language like node, python or perl to download files so this is for really rare cases.
+
+By default, release builds are ~200kb or less on windows and linux. After building with `--release`, run UPX on the result to chop off another 100kb+: `upx --best --lzma target/release/pull`
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::{env, process::exit, fs, io::{stdout, Write}};
-
-use reqwest::blocking::Client;
+use std::{env, process::exit, fs, io::{stdout, Write, self, BufRead}, net::TcpStream};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,12 +6,48 @@ fn main() {
         println!("args: url [outputfilename]");
         exit(1);
     }
-    let client = Client::new();
-    let bytes = client.get(&args[1]).send().and_then(|r| r.bytes()).expect("failed to call url");
+
+    let url = &args[1];
+    let url = if let Some(index) = url.find("//") {
+        url[index+2..].to_string()
+    } else { url.to_string() };
+
+    let (host, path) = if let Some(index) = url.find("/") {
+        (url[..index].to_string(), url[index..].to_string())
+    } else {
+        (url.to_string(), "/".into())
+    };
+    let (host, port) = if let Some(index) = host.find(":") {
+        (host[..index].to_string(), host[index+1..].parse::<usize>().expect("invalid url"))
+    } else {
+        (host.to_string(), 80 as usize)
+    };
+
+    let mut stream = TcpStream::connect(format!("{}:{}", host, port)).unwrap();
+
+    let req = format!("GET {} HTTP/1.0\r\nHost: {}\r\n\r\n", path, host);
+
+    stream.write(&req.as_bytes()).unwrap();
+    let mut resp = io::BufReader::new(stream);
+
+    let mut result: Vec<u8> = Vec::new();
+    let mut post_headers = false;
+    loop {
+        let mut buf = Vec::new();
+        resp.read_until(10, &mut buf).expect("read failure");
+        if buf.len() == 0 {
+            break;
+        } else if buf.len() == 2 && buf[0] == 13 && buf[1] == 10 {
+            post_headers = true;
+        } else if post_headers {
+            result.append(&mut buf);
+        }
+    }
+
     if args.len() > 2 {
         let path = &args[2];
-        fs::write(path, &bytes).expect("failed to write file");
+        fs::write(path, &result).expect("failed to write file");
     } else {
-        stdout().write(&bytes).expect("failed to write to stdout");
+        stdout().write(&result).expect("failed to write to stdout");
     }
 }
